de39f02ed99a4e8ca3ce30786857a33ae2415474
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-09-02 21:30:43 +0530

    Update download_link.py

--- bot/modules/download_link.py
+++ bot/modules/download_link.py
@@ -78,7 +78,7 @@ async def progress_aria(aria2, gid, event, user):
 
         except FloodWait as fd:
             LOGGER.error(fd)
-            sleep(fd.x)
+            await asyncio.sleep(fd.x)
 
         except Exception as e:
             LOGGER.error(e)
