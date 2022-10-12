9ec070394edafaa96239b739aa4b835602a0cd87
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-10-12 20:20:19 +0530

    Compressor Fix

--- bot/modules/downloader.py
+++ bot/modules/downloader.py
@@ -138,7 +138,7 @@ async def compress(local_file, out, message, user):
     else:
         total = humanbytes(os.stat(dl_loc).st_size)
         current = humanbytes(os.stat(out_loc).st_size)
-        await mess.edit(f"**Compressed Successfully!**\n\n**Name** : `{filename}`\n**Original : `{total}`\n**Compressed : `{current}`**")
+        await mess.edit(f"**Compressed Successfully!**\n\n**Name** : `{filename}`\n**Original** : `{total}`\n**Compressed** : `{current}`")
     await upload(out)
 
 
