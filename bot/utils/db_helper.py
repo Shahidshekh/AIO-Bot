3adcd05a412dbb8db6e8a69689e983bcf0c86b9a
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-10-17 16:06:11 +0530

    db

--- bot/utils/db_helper.py
+++ bot/utils/db_helper.py
@@ -1,33 +1,17 @@
-from psycopg2 import connect, DatabaseError
+from pymongo import MongoClient
 from bot.modules.logger import LOGGER
 
 
 class Database:
     def __init__(self):
         self.err=False
-        self.connect()
-        self.db_init()
+        self.coll = self.connect()
 
     def connect(self):
         try:
-            self.conn = connect("mongodb+srv://minato:minato5647@cluster0.mukmldp.mongodb.net/?retryWrites=true&w=majority")
-            self.cur = self.conn.cursor()
-        except DatabaseError as error:
-            LOGGER.error(f"Error in DB connection: {error}")
-            self.err = True
-
-    def db_init(self):
-        if self.err:
-            return
-        sql = """CREATE TABLE IF NOT EXISTS users (
-                 uid bigint,
-                 sudo boolean DEFAULT FALSE,
-                 auth boolean DEFAULT FALSE,
-                 media boolean DEFAULT FALSE,
-                 doc boolean DEFAULT FALSE,
-                 thumb bytea DEFAULT NULL,
-              )
-              """
-        self.cur.execute(sql)
-        self.conn.commit()
-        LOGGER.info("Database Active!")
+            client = MongoClient("mongodb+srv://minato:minato5647@cluster0.mukmldp.mongodb.net/?retryWrites=true&w=majority")
+            db = client["aio_bot_users"]
+            collection = db['users']
+            return collection
+        except Exception as e:
+            LOGGER.error(e)
