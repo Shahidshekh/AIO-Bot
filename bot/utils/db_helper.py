c924a99072be6c509e85cc1940d5cf37322c629b
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-10-17 18:25:03 +0530

    ds

--- bot/utils/db_helper.py
+++ bot/utils/db_helper.py
@@ -13,15 +13,15 @@ class Database:
             LOGGER.info("Connected!")
             db = client["aio_bot_users"]
             collection = db['users']
-            return collection
+            return db
         except Exception as e:
             LOGGER.error(e)
 
-    async def add_user(self, user_id, coll):
-        res = await coll.find_one({"_id": user_id})
+    async def add_user(self, user_id, db):
+        res = await db.find_one({"_id": user_id})
         LOGGER.info("here")
         if not res:
-            await coll.insert_one({"_id": user_id})
+            await db.insert_one({"_id": user_id})
             LOGGER.info("Added!")
         else:
             LOGGER.info("Exist!")
