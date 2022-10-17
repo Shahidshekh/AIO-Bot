79a08e9041e0cd369d093c0ab11c8c102ef21ec1
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-10-17 19:12:36 +0530

    ds

--- bot/database/db_client.py
+++ bot/database/db_client.py
@@ -1,4 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from bot.modules.logger import LOGGER
 
 mongodb = AsyncIOMotorClient("mongodb+srv://minato:minato5647@cluster0.mukmldp.mongodb.net/?retryWrites=true&w=majority")
 db = mongodb['aio_bot']
