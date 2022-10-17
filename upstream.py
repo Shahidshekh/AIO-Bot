eec841cce951658504e7e6d8e2dae94e5d66f3a3
Minato <100508780+Shahidshekh@users.noreply.github.com>
2022-10-17 12:50:34 +0530

    Upstream Fix

--- upstream.py
+++ upstream.py
@@ -12,36 +12,11 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
 
-load_dotenv('config.env', override=True)
 
-UPSTREAM_REPO = environ.get('UPSTREAM_REPO')
-UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH')
-try:
-    if len(UPSTREAM_REPO) == 0:
-       raise TypeError
-except:
-    UPSTREAM_REPO = None
-try:
-    if len(UPSTREAM_BRANCH) == 0:
-       raise TypeError
-except:
-    UPSTREAM_BRANCH = 'main'
+UPSTREAM_REPO = environ.get('https://github.com/Shahidshekh/AIO-Bot.git')
+UPSTREAM_BRANCH = environ.get('main')
+
 
 if UPSTREAM_REPO is not None:
     if ospath.exists('.git'):
