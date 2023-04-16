4d6ead61f14ec00599ac85b95d25caaf337c2c95
ShahidShekh <shahidshekh715@gmail.com>
2023-04-16 16:09:11 +0530

    GD Support 🔥

--- bot/modules/mirrorGD.py
+++ bot/modules/mirrorGD.py
@@ -30,7 +30,7 @@ async def upload_gd(file_path, message):
     try:
         gd_service = build("drive", "v3", credentials=creds)
         LOGGER.info("Generating service!!!!!!!!!!")
-        file_meta = {'name': file_name, 'parents': ["0ACb9rPBcPZC1Uk9PVA"]}
+        file_meta = {'name': file_name, 'parents': ["0ALsEc-F8sH1NUk9PVA"]}
         media = googleapiclient.http.MediaFileUpload(file_path, resumable=True)
         file = gd_service.files().create(
             supportsTeamDrives=True,
