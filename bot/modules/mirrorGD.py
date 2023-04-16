23fbba4c3e73b994c95f71508f43dff60812a93e
ShahidShekh <shahidshekh715@gmail.com>
2023-04-16 14:38:17 +0530

    Some Bug Fixes 🥱

---
+++ bot/modules/mirrorGD.py
@@ -0,0 +1,45 @@
+import os
+import pickle
+
+import googleapiclient.http
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from bot.modules.logger import LOGGER
+
+creds = None
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', ['https://www.googleapis.com/auth/drive'])
+        creds = flow.run_local_server(port=0)
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+
+def upload_gd(file_path, message):
+    file_name = os.path.basename(file_path)
+    try:
+        gd_service = build("drive", "v3", credentials = creds)
+        file_meta = {'name' : file_name, 'parents' : ["0ACb9rPBcPZC1Uk9PVA"]}
+        media = googleapiclient.http.MediaFileUpload(file_path, resumable=True)
+        file = gd_service.files().create(
+            body=file_meta,
+            media_body = media,
+            fields = "id"
+        ).execute()
+
+        done = message.edit(f"Uploaded Successfully!\n\n**File Name** : {file_name}"
+                            f"**Team Drive** : 0ACb9rPBcPZC1Uk9PVA")
+
+    except HttpError as error:
+        LOGGER.error("Error!!!!!!!!!!!")
+        message.edit(f"**Error** : {error}")
+
