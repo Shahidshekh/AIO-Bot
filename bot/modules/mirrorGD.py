d62e861de6e411be1cc7392863081a3dae466823
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2023-08-20 19:28:51 +0530

    Update mirrorGD.py

--- bot/modules/mirrorGD.py
+++ bot/modules/mirrorGD.py
@@ -41,7 +41,7 @@ async def upload_gd(file_path, message):
         #10u3nIOLHsVxg34h5YX0AlIDB0T1VIwrg
         #1iA4EedEkB2MX9TI3gJ7b-L3xk8jIfjwo
         await asyncio.sleep(10)
-        file_meta = {'name': file_name, 'parents': ["10u3nIOLHsVxg34h5YX0AlIDB0T1VIwrg"]}
+        file_meta = {'name': file_name, 'parents': ["1e3mT-pkz2xpeZDdbhL2Wng17u6QftxAe"]}
         media = googleapiclient.http.MediaFileUpload(file_path, resumable=True)
         
         file = gd_service.files().create(
