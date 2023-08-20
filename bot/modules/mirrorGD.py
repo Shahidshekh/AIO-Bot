ff4a51ce1dc8b47e61cff4a9426dd810015315c2
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2023-08-20 16:08:28 +0530

    Update mirrorGD.py

--- bot/modules/mirrorGD.py
+++ bot/modules/mirrorGD.py
@@ -19,7 +19,7 @@ if not creds or not creds.valid:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', ['https://www.googleapis.com/auth/drive'])
+            'service_acc.json', ['https://www.googleapis.com/auth/drive'])
         creds = flow.run_local_server(port=0)
     with open('token.pickle', 'wb') as token:
         pickle.dump(creds, token)
